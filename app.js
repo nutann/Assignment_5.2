fef4c88511def892d0216265a0710b81a6699feb
nutnaik <nutnaik@cisco.com>
2016-11-22 19:42:44 +0530

    Assignment 5.2

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var obj = {
+	name : "Nutan",
+	age : 24,
+	dob : new Date("10 April,2016"),
+	pob : "Honavar"
+}
+
+console.log("Name : " +obj.name);
+console.log("age : " +obj.age);
+console.log("dob : " +obj.dob);
+console.log("pob : " +obj.pob);
